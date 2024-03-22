4f5390ad1f36e6dadea831bfe2bf8b4b242d8e9e
AbdulHafez <abodeku508@gmail.com>
2024-03-22 16:36:28 +0300

    Adding the main tables with their controllers/models/seeders

---
+++ database/migrations/2024_03_22_125611_create_job_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('job_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained();
+            $table->foreignId('job_type_id')->constrained();
+            $table->foreignId('experience_level_id')->constrained();
+            $table->foreignId('remote_id')->constrained();
+            $table->integer('salary');
+            $table->string('location')->nullable();
+            $table->text('job_description');
+            $table->text('requirements');
+
+            $table->timestamps();
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_details');
+    }
+};
