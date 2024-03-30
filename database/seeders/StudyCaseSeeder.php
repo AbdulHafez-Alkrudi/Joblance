b6653068c539730f305b864ec9a7e7a3834c9df1
AbdulHafez <abodeku508@gmail.com>
2024-03-30 17:17:42 +0300

    Addin StudyCase seeder

--- database/seeders/StudyCaseSeeder.php
+++ database/seeders/StudyCaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\StudyCase;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,26 @@ class StudyCaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        StudyCase::create([
+            'name_EN' => 'High school',
+            'name_AR' => 'ثانوية'
+        ]);
+        StudyCase::create([
+            'name_EN' => 'Under graduated',
+            'name_AR' => 'طالب جامعي'
+        ]);
+        StudyCase::create([
+            'name_EN' => 'Graduated',
+            'name_AR' => 'خريج جامعي'
+        ]);
+        StudyCase::create([
+            'name_EN' => 'Master',
+            'name_AR' => 'ماجستير'
+        ]);
+        StudyCase::create([
+            'name_EN' => 'PHD',
+            'name_AR' => 'دكنوراه'
+        ]);
+
     }
 }
