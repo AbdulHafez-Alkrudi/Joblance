632f875e8de286c7dd52f72932c1c11aa9afc459
AbdulHafez <abodeku508@gmail.com>
2024-04-02 13:37:21 +0300

    Adding the relationships in the models

--- app/Models/StudyCase.php
+++ app/Models/StudyCase.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class StudyCase extends Model
 {
@@ -12,4 +13,9 @@ class StudyCase extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function freelancers() : HasMany
+    {
+        return $this->hasMany(Freelancer::class);
+    }
 }
